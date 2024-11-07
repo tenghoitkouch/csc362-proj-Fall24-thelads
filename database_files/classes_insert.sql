b54ac0f977a841418cace96cbd0b45272c31fe45
tenghoitkouch <159178066+tenghoitkouch@users.noreply.github.com>
2024-11-07 15:44:44 -0500

    Add files via upload

---
+++ database_files/classes_insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO classes (course_id, 
+                    section, 
+                    term_id, 
+                    professor_id,
+                    building_name, 
+                    room_number, 
+                    meeting_days_id, 
+                    time_start, 
+                    time_end
+                    )
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+
+--make section 'a'
+--if want to edit class_capacity, do on alters
