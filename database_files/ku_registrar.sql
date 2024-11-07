b58b9c78a119c932fef3ee6881090886dc78f85f
tenghoitkouch <159178066+tenghoitkouch@users.noreply.github.com>
2024-11-07 15:46:57 -0500

    Update ku_registrar.sql

--- database_files/ku_registrar.sql
+++ database_files/ku_registrar.sql
@@ -1,6 +1,10 @@
 DROP DATABASE IF EXISTS ku_registrar;
 CREATE DATABASE ku_registrar;
 
-USE red_river_climbs;
+USE ku_registrar;
+
 
 --insert creates
+SOURCE creates/classes.sql;
+SOURCE creates/degree_requirements.sql;
+SOURCE creates/student_class_history.sql;
