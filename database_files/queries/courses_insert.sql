6969dc4f8e9b93c000a74423ce30eb5c5c3dbed1
Varnern23 <159178249+Varnern23@users.noreply.github.com>
2024-11-10 21:40:48 -0500

    Create courses_insert.sql

---
+++ database_files/queries/courses_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO courses(course_discipline,
+                    course_number,
+                    course_credits,
+                    course_description
+                    );
+VALUES (?,?,?,?);
