b468e26b4bd80cc5b91dd3f9e0f3ebc1197594f4
Varnern23 <159178249+Varnern23@users.noreply.github.com>
2024-11-10 20:57:36 -0500

    Create degrees.sql

---
+++ database_files/creates/degrees.sql
@@ -0,0 +1,12 @@
+CREATE TABLE degrees (
+  degree_id    INT AUTO_INCREMENT,
+  degree_name  VARCHAR(75),
+  PRIMARY KEY (degree_id)
+)
+CREATE VIEW degrees_view (
+  SELECT  degree_id,
+          degree_name
+  FROM    degrees
+  GROUP BY degree_id ASC
+  ORDER BY degree_name ASC
+)
