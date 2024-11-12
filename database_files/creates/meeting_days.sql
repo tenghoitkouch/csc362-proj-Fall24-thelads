e955498bc6379a6ed0c91e049fb7cff4f356fd4d
CW24CS <159178009+CW24CS@users.noreply.github.com>
2024-11-12 09:48:52 -0500

    Create meeting_days.sql

---
+++ database_files/creates/meeting_days.sql
@@ -0,0 +1,6 @@
+CREATE TABLE meeting_days(
+  meeting_days_id  INT AUTO_INCREMENT,
+  day_letter       CHAR(8),
+  PRIMARY KEY (meeting_days_id, day_letter),
+  FOREIGN KEY (day_letter) REFERENCES days (day_letter) ON DELETE RESTRICT,
+);
