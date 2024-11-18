a7c927c82423e2b28aad9169c994c59cf9b81b4e
orange <tenghoit.kouch@centre.edu>
2024-11-18 01:06:46 +0000

    small change

--- database_files/html/class_catalog.php
+++ database_files/html/class_catalog.php
@@ -81,7 +81,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Class Catalog</title>
     <?php
         if($_COOKIE[$mode] == $light){
             ?><link rel="stylesheet" href="../css/basic.css"><?php
@@ -91,7 +91,7 @@
     ?>
 </head>
 <body>
-    <h1>Title</h1>
+    <h1>Class Catalog</h1>
     <form method="post">
         <p><input type="submit" name="toggle_mode" value="Toggle Light/Dark Mode" /></p>
     </form>
