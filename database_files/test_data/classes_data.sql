f5cf528749b15860d845f8866adfdfd03e010fa3
orange <tenghoit.kouch@centre.edu>
2024-11-21 19:49:13 +0000

    more test data

---
+++ database_files/test_data/classes_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO classes (course_id, 
+                    section, 
+                    term_id, 
+                    professor_id,
+                    building_name, 
+                    room_number, 
+                    meeting_days_id, 
+                    time_start, 
+                    time_end
+                    )
+VALUES  (1, "a", 1, 1, "OLIN", 201, 6, "08:00:00", "09:00:00"),
+        (2, "a", 1, 1, "OLIN", 201, 6, "09:10:00", "10:10:00");
---
+++ database_files/test_data/meeting_times_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO meeting_times (time_start, time_end)
+VALUES  ("08:00:00", "09:00:00"),
+        ("09:10:00", "10:10:00");
+
