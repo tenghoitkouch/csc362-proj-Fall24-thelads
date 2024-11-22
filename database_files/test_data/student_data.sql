6ac04f0f619d918075441f9b4ae349fd292b0589
Varnern23 <159178249+Varnern23@users.noreply.github.com>
2024-11-22 04:38:57 -0500

    Create student_data.sql

---
+++ database_files/test_data/student_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO students (student_id, student_first_name, student_last_name, student_email, student_phone_number, student_street, student_city, student_state, student_zip_code, professor_id)
+VALUES 
+("1", "jeff", "green", "jeff.green@ku.edu", "(502)-438-2902", "1321, 9th Street", "Louisville", "KY", "40291", 1);
