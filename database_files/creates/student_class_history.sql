a6cc54ac44450b3b52e6cbba77db69ac2058a0cc
orange <tenghoit.kouch@centre.edu>
2024-12-08 22:25:58 +0000

    time confilct is not done

--- database_files/creates/student_class_history.sql
+++ database_files/creates/student_class_history.sql
@@ -79,14 +79,14 @@ BEGIN
     END IF;
 
     -- time constraint
-    SET @current_term_id = get_term_id_by_class(NEW.class_id);
-    SET @current_time_start = get_time_start_by_class(NEW.class_id);
-    SET @current_time_end = get_time_end_by_class(NEW.class_id);
-    SET @num_classes_at_current_time = get_num_student_class_by_term_time(@current_term_id, @current_time_start, @current_time_end, NEW.student_id);
+    -- SET @current_term_id = get_term_id_by_class(NEW.class_id);
+    -- SET @current_time_start = get_time_start_by_class(NEW.class_id);
+    -- SET @current_time_end = get_time_end_by_class(NEW.class_id);
+    -- SET @num_classes_at_current_time = get_num_student_class_by_term_time(@current_term_id, @current_time_start, @current_time_end, NEW.student_id);
 
-    IF (@num_classes_at_current_time <> 0) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Already enrolled in another class at that time';
-    END IF;
+    -- IF (@num_classes_at_current_time <> 0) THEN
+    --     SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Already enrolled in another class at that time';
+    -- END IF;
 
 END; $$
 DELIMITER ;        
@@ -108,19 +108,19 @@ BEGIN
     END IF;     
 
     -- time constraint
-    SET @current_term = get_term_id_by_class(NEW.class_id);
-    SET @new_time_start = get_time_start_by_class(NEW.class_id);
-    SET @new_time_end = get_time_end_by_class(NEW.class_id);
-    SET @num_classes_at_current_time = get_num_student_class_by_term_time(@current_term, @new_time_start, @new_time_end, NEW.student_id);
+    -- SET @current_term = get_term_id_by_class(NEW.class_id);
+    -- SET @new_time_start = get_time_start_by_class(NEW.class_id);
+    -- SET @new_time_end = get_time_end_by_class(NEW.class_id);
+    -- SET @num_classes_at_current_time = get_num_student_class_by_term_time(@current_term, @new_time_start, @new_time_end, NEW.student_id);
 
-    SET @old_time_start = get_time_start_by_class(OLD.class_id);
-    SET @old_time_end = get_time_end_by_class(OLD.class_id);
+    -- SET @old_time_start = get_time_start_by_class(OLD.class_id);
+    -- SET @old_time_end = get_time_end_by_class(OLD.class_id);
 
-    IF (@new_time_start <> @old_time_start) OR (@new_time_end <> @old_time_end) THEN
-        IF (@num_classes_at_current_time <> 0) THEN
-            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Already enrolled in another class at that time';
-        END IF; 
-    END IF; 
+    -- IF (@new_time_start <> @old_time_start) OR (@new_time_end <> @old_time_end) THEN
+    --     IF (@num_classes_at_current_time <> 0) THEN
+    --         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Already enrolled in another class at that time';
+    --     END IF; 
+    -- END IF; 
 
 END; $$
 DELIMITER ;        
