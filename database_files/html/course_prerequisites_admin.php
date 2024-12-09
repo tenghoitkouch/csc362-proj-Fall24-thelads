43c30f5f32a9fcaeb26a89c57204436760aa3dac
orange <tenghoit.kouch@centre.edu>
2024-12-09 04:55:21 +0000

    started course_prereq

---
+++ database_files/html/course_prerequisites_admin.php
@@ -0,0 +1,65 @@
+<?php
+
+    // Show all errors from the PHP interpreter.
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    // Show all errors from the MySQLi Extension.
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);  
+
+    // CONNECTION
+    $config = parse_ini_file('../../../mysql.ini');
+    $queries_dir = "../queries/";
+    $dbname = 'ku_registrar';
+    $conn = new mysqli(
+                $config['mysqli.default_host'],
+                $config['mysqli.default_user'],
+                $config['mysqli.default_pw'],
+                $dbname);
+
+    if ($conn->connect_errno) {
+        echo "Error: Failed to make a MySQL connection, here is why: ". "<br>";
+        echo "Errno: " . $conn->connect_errno . "\n";
+        echo "Error: " . $conn->connect_error . "\n";
+        exit; // Quit this PHP script if the connection fails.
+    }
+
+    // import our custom php functions
+    require "library.php";
+    session_start();
+
+    if(array_key_exists('logout', $_POST)){
+        session_unset();
+        $_SESSION['logged_in'] = FALSE;
+
+        header("Location: home.php", true, 303);
+        exit;
+    }
+
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Course Prerequisites</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Kendianawa University Registrar</h1>
+        <nav>
+            <?php build_nav(); ?>
+        </nav>
+    </header>
+    <main>
+        <h2>Course Prerequisites</h2>
+        result_to_html_table_with_checkbox($)
+    </main>    
+    <footer><p>&copy; 2024 Kendianawa University. All rights reserved.</p></footer>
+    <?php $conn->close(); ?>
+</body>
+</html>
